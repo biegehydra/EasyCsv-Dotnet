0c6294ee26aec01216370bf9b866d4882dab2adf
biegehydra <connorhallman29@gmail.com>
2024-01-28 22:47:43 -0500

    Change matching behaviour, create clones of expected headers

--- src/EasyCsv.Components/Models.cs
+++ src/EasyCsv.Components/Models.cs
@@ -17,15 +17,10 @@ public class ExpectedHeader
     /// <summary>
     /// AutoMatching will be done on all the values in this list
     /// </summary>
-    internal List<string> ValuesToMatch { get; init; }
+    internal List<string> ValuesToMatch { get; private set; }
 
     internal string DisplayName { get; init; }
-
-    /// <summary>
-    /// AutoMatching will be done on all the values in this list
-    /// </summary>
-    internal ExpectedHeaderConfig Config { get; init; }
-
+    internal ExpectedHeaderConfig Config { get; private set; }
     internal object? Value { get; set; }
     internal bool HasValue => Value is string str ? !string.IsNullOrWhiteSpace(str) : Value != null;
 
@@ -33,7 +28,7 @@ public class ExpectedHeader
     /// Expected headers represents csharp properties that you want a value for when reading a csv.
     /// Users can map a header from the csv or provide a default value or neither.
     /// </summary>
-    /// <param name="csharpPropertyName">Values from the csv column this is matched to, or from the default value provided, will be put into this property when <see cref="CsvTableHeaderMatcher{T}.GetRecords"/> is called.</param>
+    /// <param name="csharpPropertyName">Values from the csv column this is matched to, or from the default value provided, will be put into this property when <see cref="CsvTableHeaderMatcher.GetRecords"/> is called.</param>
     /// <param name="displayName">
     /// The value that will be displayed in the 'Expected Header' column of the matcher. If this matches <paramref name="csharpPropertyName"/> it will be split
     /// on capital letters. E.g. "FirstName" will be displayed as "First Name"
@@ -45,7 +40,7 @@ public class ExpectedHeader
         ValuesToMatch = valuesToMatch;
         CSharpPropertyName = csharpPropertyName;
         DisplayName = displayName;
-        if (config == null) config = ExpectedHeaderConfig.Default;
+        config ??= ExpectedHeaderConfig.Default;
         Config = config;
         Value = config.InitialDefaultValue;
     }
@@ -54,7 +49,7 @@ public class ExpectedHeader
     /// Expected headers represents csharp properties that you want a value for when reading a csv.
     /// Users can map a header from the csv or provide a default value or neither.
     /// </summary>
-    /// <param name="csharpPropertyName">Values from the csv column this is matched to, or from the default value provided, will be put into this property when <see cref="CsvTableHeaderMatcher{T}.GetRecords"/> is called.</param>
+    /// <param name="csharpPropertyName">Values from the csv column this is matched to, or from the default value provided, will be put into this property when <see cref="CsvTableHeaderMatcher.GetRecords"/> is called.</param>
     /// <param name="displayName">
     /// The value that will be displayed in the 'Expected Header' column of the matcher. If this matches <paramref name="csharpPropertyName"/> it will be split
     /// on capital letters. E.g. "FirstName" will be displayed as "First Name"
@@ -70,7 +65,8 @@ public class ExpectedHeader
         {
             var expectedHeaderConfigurator = new ExpectedHeaderConfigurator();
             configurator(expectedHeaderConfigurator);
-            var config = new ExpectedHeaderConfig(expectedHeaderConfigurator.IsRequired, expectedHeaderConfigurator.DefaultValueType, expectedHeaderConfigurator.DefaultValueRenderFragment, expectedHeaderConfigurator.InitialDefaultValue);
+            var config = new ExpectedHeaderConfig(expectedHeaderConfigurator.IsRequired, expectedHeaderConfigurator.DefaultValueType,
+                expectedHeaderConfigurator.DefaultValueRenderFragment, expectedHeaderConfigurator.InitialDefaultValue, expectedHeaderConfigurator.AutoMatching);
             Config = config;
         }
         else
@@ -85,7 +81,7 @@ public class ExpectedHeader
     /// Users can map a header from the csv or provide a default value or neither.
     /// </summary>
     /// <param name="csharpPropertyName">
-    /// Values from the csv column this is matched to, or from the default value provided, will be put into this property when <see cref="CsvTableHeaderMatcher{T}.GetRecords"/> is called.
+    /// Values from the csv column this is matched to, or from the default value provided, will be put into this property when <see cref="CsvTableHeaderMatcher.GetRecords"/> is called.
     /// <paramref name="csharpPropertyName"/> will also be used as the display name which was what's displayed in the 'Expected Header' column of the matcher,
     /// but it's value will be split on capital letters. E.g. "FirstName" will be displayed as "First Name".
     /// <paramref name="csharpPropertyName"/> will also be the only item in the <see cref="ValuesToMatch"/> list which is used to match csv headers to this.
@@ -103,7 +99,7 @@ public class ExpectedHeader
     /// Users can map a header from the csv or provide a default value or neither.
     /// </summary>
     /// <param name="csharpPropertyName">
-    /// Values from the csv column this is matched to, or from the default value provided, will be put into this property when <see cref="CsvTableHeaderMatcher{T}.GetRecords"/> is called.
+    /// Values from the csv column this is matched to, or from the default value provided, will be put into this property when <see cref="CsvTableHeaderMatcher.GetRecords"/> is called.
     /// <paramref name="csharpPropertyName"/> will also be used as the display name which was what's displayed in the 'Expected Header' column of the matcher,
     /// but it's value will be split on capital letters. E.g. "FirstName" will be displayed as "First Name".
     /// <paramref name="csharpPropertyName"/> will also be the only item in the <see cref="ValuesToMatch"/> list which is used to match csv headers to this.
@@ -114,16 +110,33 @@ public class ExpectedHeader
     {
 
     }
+
+    public bool Equals(ExpectedHeader? other)
+    {
+        if (other == null) return false;
+        return CSharpPropertyName == other.CSharpPropertyName && DisplayName == other.DisplayName &&
+               ValuesToMatch.Count == other.ValuesToMatch.Count && ValuesToMatch.All(other.ValuesToMatch.Contains) 
+               && Config.Equals(other.Config);
+    }
+
+    public ExpectedHeader DeepClone()
+    {
+        var clone = (ExpectedHeader) MemberwiseClone();
+        clone.Config = Config.Clone();
+        clone.ValuesToMatch = ValuesToMatch.ToList();
+        return clone;
+    }
 }
 public class ExpectedHeaderConfig
 {
-    internal ExpectedHeaderConfig(bool required, DefaultValueType allowDefaultValue,
-        RenderFragment<DefaultValueRenderFragmentsArg>? defaultValueRenderFragment, object? initialDefaultValue)
+    internal ExpectedHeaderConfig(bool required, DefaultValueType defaultValueType,
+        RenderFragment<DefaultValueRenderFragmentsArg>? defaultValueRenderFragment, object? initialDefaultValue, AutoMatching? autoMatching)
     {
         IsRequired = required;
-        DefaultValueType = allowDefaultValue;
+        DefaultValueType = defaultValueType;
         DefaultValueRenderFragment = defaultValueRenderFragment;
         InitialDefaultValue = initialDefaultValue;
+        AutoMatching = autoMatching;
     }
 
     /// <param name="defaultValueType">
@@ -136,8 +149,11 @@ public class ExpectedHeaderConfig
     /// <param name="initialDefaultValue">
     /// The initial default value of this expected header. Will show up in whatever input type is used in the "Default Value" column.
     /// </param>
-    public ExpectedHeaderConfig(DefaultValueType defaultValueType = DefaultValueType.None, bool required = false, object? initialDefaultValue = null) :
-        this(required, defaultValueType, null, initialDefaultValue) { }
+    /// <param name="autoMatching">
+    /// Set the auto matching level for this specific property
+    /// </param>
+    public ExpectedHeaderConfig(DefaultValueType defaultValueType = DefaultValueType.None, bool required = false, object? initialDefaultValue = null, AutoMatching? autoMatching = null) :
+        this(required, defaultValueType, null, initialDefaultValue, autoMatching) { }
 
     /// <param name="defaultValueRenderFragment">
     /// A custom element that will be used in the "Default Value" column. This will override the DefaultValueType.
@@ -149,51 +165,88 @@ public class ExpectedHeaderConfig
     /// <param name="initialDefaultValue">
     /// The initial default value of this expected header. Will show up in whatever input type is used in the "Default Value" column.
     /// </param>
-    public ExpectedHeaderConfig(RenderFragment<DefaultValueRenderFragmentsArg>? defaultValueRenderFragment, bool required = false, object? initialDefaultValue = null) :
-        this(required, DefaultValueType.None, defaultValueRenderFragment, initialDefaultValue)
+    /// <param name="autoMatching">
+    /// Set the auto matching level for this specific property
+    /// </param>
+    public ExpectedHeaderConfig(RenderFragment<DefaultValueRenderFragmentsArg>? defaultValueRenderFragment, bool required = false, object? initialDefaultValue = null, AutoMatching? autoMatching = null) :
+        this(required, DefaultValueType.None, defaultValueRenderFragment, initialDefaultValue, autoMatching)
     {
         
     }
+
+    /// <summary>
+    /// Sets the auto matching level for this specific property
+    /// </summary>
+    public AutoMatching? AutoMatching { get; private init; }
 
     /// <summary>
     /// If required, a default value must be provided or a header must be matched to the expected header with this config for the matcher to be considered valid.
     /// </summary>
     public bool IsRequired { get; private init; }
     /// <summary>
-    /// Determines whether or
+    /// Determines what input component is used to provide a default value
     /// </summary>
     public DefaultValueType DefaultValueType { get; private init; }
+    /// <summary>
+    /// The initial default value
+    /// </summary>
     public object? InitialDefaultValue { get; set; }
+    /// <summary>
+    /// Allows you to provide a custom default value component
+    /// </summary>
     public RenderFragment<DefaultValueRenderFragmentsArg>? DefaultValueRenderFragment { get; private init; }
     /// <summary>
     /// A default value will NOT be allowed. A csv header can be mapped but is not required for the matcher to be considered valid. 
     /// </summary>
-    public static readonly ExpectedHeaderConfig Default = new ExpectedHeaderConfig();
+    public static readonly ExpectedHeaderConfig Default = new ();
     /// <summary>
     ///  A default value will NOT be allowed. A csv header must be mapped or the matcher will be considered invalid.
     /// </summary>
-    public static readonly ExpectedHeaderConfig Required = new ExpectedHeaderConfig(required: true);
+    public static readonly ExpectedHeaderConfig Required = new (required: true);
     /// <summary>
     /// A default value with a text input will be allowed. A csv header can also be mapped but neither are required for the matcher to be considered valid. 
     /// </summary>
-    public static readonly ExpectedHeaderConfig TextDefaultValue = new ExpectedHeaderConfig(defaultValueType: DefaultValueType.Text);
+    public static readonly ExpectedHeaderConfig TextDefaultValue = new (defaultValueType: DefaultValueType.Text);
     /// <summary>
     /// A default value with a text input will be allowed. A csv header can also be mapped, one of either are required for the matcher to be considered valid. 
     /// </summary>
-    public static readonly ExpectedHeaderConfig RequiredTextDefaultValue = new ExpectedHeaderConfig(required: true, defaultValueType: DefaultValueType.None);
+    public static readonly ExpectedHeaderConfig RequiredTextDefaultValue = new (required: true, defaultValueType: DefaultValueType.None);
+
+    public ExpectedHeaderConfig Clone()
+    {
+        return (ExpectedHeaderConfig) MemberwiseClone();
+    }
+
+    public bool Equals(ExpectedHeaderConfig? other)
+    {
+        if (other == null) return false;
+        return AutoMatching == other.AutoMatching && IsRequired == other.IsRequired &&
+               DefaultValueType == other.DefaultValueType && InitialDefaultValue == other.InitialDefaultValue &&
+               DefaultValueRenderFragment == other.DefaultValueRenderFragment;
+    }
 }
 
 public class ExpectedHeaderConfigurator
 {
     /// <summary>
+    /// Sets the auto matching level for this specific property
+    /// </summary>
+    public AutoMatching? AutoMatching { get; private init; }
+    /// <summary>
     /// If required, a default value must be provided or a header must be matched to the expected header with this config for the matcher to be considered valid.
     /// </summary>
     public bool IsRequired { get; set; }
     /// <summary>
-    /// Determines whether or
+    /// Determines what input component is used to provide a default value
     /// </summary>
     public DefaultValueType DefaultValueType { get; set; }
+    /// <summary>
+    /// Allows you to provide a custom default value component
+    /// </summary>
     public RenderFragment<DefaultValueRenderFragmentsArg>? DefaultValueRenderFragment { get; set; }
+    /// <summary>
+    /// The initial default value
+    /// </summary>
     public object? InitialDefaultValue { get; set; }
 }
 

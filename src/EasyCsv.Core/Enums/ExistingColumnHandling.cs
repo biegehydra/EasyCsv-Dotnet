03c79a1c7a84fd3a77bcbe22a22353e8c5e1c5aa
biegehydra <connorhallman29@gmail.com>
2024-04-27 20:42:45 -0400

    Existing column handling

---
+++ src/EasyCsv.Core/Enums/ExistingColumnHandling.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace EasyCsv.Core.Enums;
+public enum ExistingColumnHandling
+{
+    [Description("If the specified column already exists, the value will be overriden in every row.")]
+    Override,
+    [Description("If the specified column already exists, the value in a row will be ovverriden if it is null or whitespace.")]
+    ReplaceNullOrWhiteSpace,
+    [Description("If the specified column already exists, the value will not be changed in any row.")]
+    Keep,
+    [Description("An exception will be thrown if the specified column already exists")]
+    ThrowException
+}

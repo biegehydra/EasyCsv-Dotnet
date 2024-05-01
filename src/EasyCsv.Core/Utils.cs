cd7a3f5e3e3693b18db9fc051b0fccce2cca8439
biegehydra <connorhallman29@gmail.com>
2024-04-30 21:08:01 -0400

    Create ReferenceColumnName select and Names

---
+++ src/EasyCsv.Core/Utils.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace EasyCsv.Core;
+
+public static class Utils
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsValidIndex(int index, int size)
+    {
+        return index >= 0 && index < size;
+    }
+}

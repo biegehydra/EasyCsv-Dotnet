c5e8632fa649820ebd0d3cb96c578688f6e0b52a
biegehydra <connorhallman29@gmail.com>
2024-05-07 06:02:52 -0400

    Change to full csv operations and column operations

--- src/EasyCsv.Core/InternalColumnNames.cs
+++ src/EasyCsv.Core/InternalColumnNames.cs
@@ -3,4 +3,5 @@ public static class InternalColumnNames
 {
     public const string Tags = "EasyCsvTags";
     public const string References = "EasyCsvReferences";
+    public const string FullCsvOperations = "_FullCsvOperations";
 }
